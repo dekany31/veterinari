9d5c782d383f3f05498a137c2187ff1deb684cd6
dekany31 <66486271+dekany31@users.noreply.github.com>
2021-04-26 17:55:21 +0200

    No: 5 commit. this is a 1200 seconds auto commit. Mon 04/26/2021 - 17:35:20.69

--- src/components/Pet.jsx
+++ src/components/Pet.jsx
@@ -2,7 +2,35 @@ import React, {useState} from 'react'
 
 function Pet(props) {
 
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(props.petdata.isVaccinated)
+  const [data, setData] = useState([])
+//  const [loading, setLoading] = useState(false)
+//  const [ already, setAlready ] = useState(null);
+  const [ response, setResponse ] = useState(null);
+  const [show, setShow] = useState(false);
+
+const savePats = () => {
+  fetch('/api/pets',
+  { method: 'POST',
+    mode: 'cors',
+    headers: {
+    'Content-Type': 'application/json'
+             },
+    body: JSON.stringify({
+        name: props.petdata.name,
+        isVaccinated: toggle
+    })
+  })
+  .then( (resp) => (setResponse(true)) )
+  .then ( (adat) => {console.log(adat); setData(adat) })
+  .catch( (err) => {
+                    console.log("FetchError=", err);
+                    setResponse(false);
+                  } 
+        )
+  .finally( () => setTimeout(() => setShow(false), 0) )
+}
+
 
   console.log("pets =", props.petdata.isVaccinated)
   return (
@@ -10,9 +38,10 @@ function Pet(props) {
       <p>{props.petdata.name}({props.petdata.animal}) - "Vaccinated:" 
       
         
-      <button onClick={() => setToggle(!setToggle)}>
-         {props.petdata.isVaccinated ? "true"
-                                    : "false"
+      <button onClick={() => setToggle(!toggle)}>
+         {show ? "..."
+                :toggle ? "true"
+                        : "false"
          }
       </button>
                               
@@ -22,5 +51,6 @@ function Pet(props) {
     </div>
   )
 }
+
 
 export default Pet
